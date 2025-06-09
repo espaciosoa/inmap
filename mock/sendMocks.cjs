16f8570bb958c9f372f4256a33f9ddb54237f92d
alreylz <alreylop@gmail.com>
2025-06-09 11:44:35 +0200

    Improved mock testing by generating a bunch a measurements of diverse types. I can definitely generate now test data for 5G if the phone never actually manages to connect to pure 5G infraestructure. Added new db maintenance methods to simplify testing and detecting unlinked data in the database

--- mock/sendMocks.cjs
+++ mock/sendMocks.cjs
@@ -21,7 +21,22 @@ async function postData(subpath, bodyObj) {
     return result
 }
 
-async function main() {
+async function main(options = {
+    numMeasurements: 1000,
+    sessionsGeneratorConfig: [
+        {
+            lat: 40.43419962968242,
+            lon: -3.626855970671248,
+            comment:"Mock session with coords at workplace"
+        },
+        // {
+        //     lat:40.33222093350904, 
+        //     lon:-3.766959080089313,
+        //     comment:"Mock session with coords at Uni"
+
+        // }
+    ]
+}) {
 
     // Creating a room from template
     const room_MockTemplate = require('./templates/room.MockTemplate.json')
@@ -33,30 +48,59 @@ async function main() {
     // Creating a room measurement from template
     const measurementSessionForRoom_MockTemplate = require(`./templates/measurementSession.MockTemplate.json`)
     delete measurementSessionForRoom_MockTemplate._id
-    measurementSessionForRoom_MockTemplate.roomId = nuRoom._id;
-    measurementSessionForRoom_MockTemplate.measuremensOwner = "MOCK_ESPACIOSOA"
-    measurementSessionForRoom_MockTemplate.measurementDevice = "ANDROID_PHONE"
-    measurementSessionForRoom_MockTemplate.timestamp = (new Date()).toISOString();
-    const nuMeasurementSession = await postData("/measurementSessions", measurementSessionForRoom_MockTemplate)
-    console.log("INSERTED A SESSION", nuMeasurementSession)
+    for (const sessionGenConfig of options.sessionsGeneratorConfig) {
+        const newMeasurementSessionForRoom_MockTemplate = structuredClone(measurementSessionForRoom_MockTemplate);
+        newMeasurementSessionForRoom_MockTemplate.roomId = nuRoom._id;
+        newMeasurementSessionForRoom_MockTemplate.measuremensOwner = "MOCK_ESPACIOSOA"
+        newMeasurementSessionForRoom_MockTemplate.measurementDevice = "ANDROID_PHONE"
+        newMeasurementSessionForRoom_MockTemplate.timestamp = (new Date()).toISOString();
+        newMeasurementSessionForRoom_MockTemplate.worldPosition = { lat: sessionGenConfig.lat, lon: sessionGenConfig.lon };
+        newMeasurementSessionForRoom_MockTemplate.comment = sessionGenConfig.comment ?? "No comment"
+        const nuMeasurementSession = await postData("/measurementSessions", newMeasurementSessionForRoom_MockTemplate)
+        console.log("INSERTED A SESSION", nuMeasurementSession)
 
-    //Creating a measurement (phone template)
-    const phoneMeasurementForSessionInRoom_MockTemplate = require("./templates/phoneRoomMeasurement.MockTemplate.json")
-    delete phoneMeasurementForSessionInRoom_MockTemplate._id
-    phoneMeasurementForSessionInRoom_MockTemplate.timestamp = (new Date()).toISOString();
-    phoneMeasurementForSessionInRoom_MockTemplate.roomId = nuRoom._id
-    phoneMeasurementForSessionInRoom_MockTemplate.measurementSession = nuMeasurementSession._id
-    const nuPhoneMeasurement = await postData("/roomMeasurements", phoneMeasurementForSessionInRoom_MockTemplate)
-    console.log("INSERTED A PHONE Measurement", nuPhoneMeasurement)
 
-    // Creating a PI Measurement (phone template)
-    const piMeasurementForRoom_MockTemplate = require('./templates/piRoomMeasurement.MockTemplate.json');
-    delete piMeasurementForRoom_MockTemplate._id
-    piMeasurementForRoom_MockTemplate.timestamp = (new Date()).toISOString();
-    piMeasurementForRoom_MockTemplate.roomId = nuRoom._id
-    const nuPiMeasurement = await postData("/roomMeasurements", piMeasurementForRoom_MockTemplate)
-    console.log("INSERTED A PI Measurement", nuPiMeasurement)
 
+        //Creating a measurement (phone template)
+        const phoneMeasurementForSessionInRoom_MockTemplate = require("./templates/phoneRoomMeasurement.MockTemplate.json")
+        delete phoneMeasurementForSessionInRoom_MockTemplate._id
+
+
+        for (let times = 0; times < options.numMeasurements; times++) {
+            const newPhoneMeasurementForSessionInRoom_FromMockTemplate = structuredClone(phoneMeasurementForSessionInRoom_MockTemplate)
+
+            const genRandomPositionInArea = (radiusMeters) => {
+                const angle = Math.random() * 2 * Math.PI;
+                const radius = Math.sqrt(Math.random()) * radiusMeters;
+                const x = radius * Math.cos(angle);
+                const z = radius * Math.sin(angle);
+                return {
+                    x: x,
+                    y: phoneMeasurementForSessionInRoom_MockTemplate.position.y,
+                    z: z
+                }
+            }
+
+            //Create N phone mock measurements 
+            newPhoneMeasurementForSessionInRoom_FromMockTemplate.position = genRandomPositionInArea(10)
+            newPhoneMeasurementForSessionInRoom_FromMockTemplate.timestamp = (new Date()).toISOString();
+            newPhoneMeasurementForSessionInRoom_FromMockTemplate.roomId = nuRoom._id
+            newPhoneMeasurementForSessionInRoom_FromMockTemplate.measurementSession = nuMeasurementSession._id
+
+            const nuPhoneMeasurement = await postData("/roomMeasurements", newPhoneMeasurementForSessionInRoom_FromMockTemplate)
+            console.log("INSERTED A PHONE Measurement ", nuPhoneMeasurement)
+        }
+        console.log(`DONE Mocking ${options.numMeasurements} ROOM MEASUREMENTS`)
+
+        // Creating a PI Measurement (phone template)
+        const piMeasurementForRoom_MockTemplate = require('./templates/piRoomMeasurement.MockTemplate.json');
+        delete piMeasurementForRoom_MockTemplate._id
+        piMeasurementForRoom_MockTemplate.timestamp = (new Date()).toISOString();
+        piMeasurementForRoom_MockTemplate.roomId = nuRoom._id
+        const nuPiMeasurement = await postData("/roomMeasurements", piMeasurementForRoom_MockTemplate)
+        console.log("INSERTED A PI Measurement", nuPiMeasurement)
+
+    }
 }
 
 
